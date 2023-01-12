c1b2fc7b279b6a1deeb8a22a561397916791f80c
Leo Mayuzumi <leoleomayuzumi@gmail.com>
2023-01-13 00:47:51 +0900

    ✨ header作成

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <!-- header -->
+    <div class="relative">
+      <img class="w-screen" src="./assets/photos/DSC09688.JPG" />
+      <Typography class="absolute font-semibold text-5xl text-white top-[75%] left-[15%]" text="Leo Mayuzumi"/>
+    </div>
+  </div>
 </template>
---
+++ components/Typography.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="font-notoSans">{{ props.text }}</p>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  text: ""
+})
+</script>
