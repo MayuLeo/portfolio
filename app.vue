d22eecfde07926ff5d44b77d2cfc83fc7cff0132
Leo Mayuzumi <leoleomayuzumi@gmail.com>
2023-01-16 02:30:14 +0900

    skillを追記した

--- app.vue
+++ app.vue
@@ -23,10 +23,6 @@
               class="text-2xl text-gray-700"
               text="黛 礼雄 / Mayuzumi Leo"
             />
-            <!-- <Typography
-              class="text-xl text-gray-700"
-              :text="'氏名：黛 礼雄\n生年月日：1997/07/31\n出身・在住：東京都・三重県\n資格：応用情報処理技術者'"
-            /> -->
             <br />
             <Typography
               class="text-gray-700"
@@ -43,32 +39,81 @@
       <!-- skill -->
       <div class="flex flex-col py-16 px-2 w-[960px] gap-y-4">
         <Typography class="text-4xl font-semibold text-gray-700" text="Skill" />
+
+        <Typography
+          class="text-2xl font-semibold text-gray-700 pl-2 pt-6"
+          text="Language"
+        />
         <div class="flex w-full gap-x-16 px-4">
           <div class="flex flex-col gap-y-8 w-full">
             <SkillBar
-              title="C#"
-              :description="'高専プロコンの競技部門で初めて経験。\n就職後、.Net CoreでのWebAPI作成にて3年間使用。'"
-              :use-period-year="8"
+              title="Nuxt"
+              :description="'就職して初めてのフロントエンドがNuxtでした。現在はNuxt3を用いた業務用アプリケーションの作成を担当しています。'"
+              :use-period-year="2"
             />
             <SkillBar
-              title="Nuxt"
-              :description="'aaaaaaaa'"
-              :use-period-year="0.5"
+              title="Python"
+              :description="'学生時代に簡単なSVMを作ったり、Raspberry Piでモーターを制御したりしてました。'"
+              :use-period-year="3"
             />
           </div>
           <div class="flex flex-col gap-y-8 w-full">
             <SkillBar
-              title="C#1"
-              :description="'高専プロコンの競技部門で初めて経験。\n就職後、.Net CoreでのWebAPI作成にて3年間使用。'"
+              title="C#"
+              :description="'高専プロコンの競技部門で初めて経験。\n就職後、.Net CoreでのWebAPI作成やチャットボットの作成で利用しています。'"
               :use-period-year="5"
             />
             <SkillBar
-              title="Nuxt1"
-              :description="'aaaaaaaa'"
-              :use-period-year="5"
+              title="C言語"
+              :description="'高専1年生で初めて習った言語がC言語でした。大学では授業の課題でコンパイラを半年間かけて作成しました。'"
+              :use-period-year="3"
             />
           </div>
         </div>
+
+        <!--  -->
+        <Typography
+          class="text-2xl font-semibold text-gray-700 pl-2 pt-6"
+          text="OS"
+        />
+        <div class="flex w-full gap-x-16 px-4">
+          <div class="flex flex-col gap-y-8 w-full">
+            <SkillBar
+              title="Windows / Windows Server"
+              :description="'小学生の頃からパソコン好きな父の影響で触ってきました。業務ではAVDやActiveDirectory周りを担当しました。'"
+              :use-period-year="7"
+            />
+          </div>
+          <div class="flex flex-col gap-y-8 w-full">
+            <SkillBar
+              title="macOS"
+              :description="'高専プロコン参加時に学校から借りて以来、Macを愛用しています。最近入れてよかったアプリはRaycastです。'"
+              :use-period-year="8"
+            />
+          </div>
+        </div>
+
+        <Typography
+          class="text-2xl font-semibold text-gray-700 pl-2 pt-6"
+          text="Other"
+        />
+        <div class="flex w-full gap-x-16 px-4">
+          <div class="flex flex-col gap-y-8 w-full">
+            <SkillBar
+              title="Azure"
+              :description="'就職して初めてクラウドを利用しました。一番好きなサービスはQnA Makerです。（もう廃止されてしまいますが…）'"
+              :use-period-year="3"
+            />
+          </div>
+          <div class="flex flex-col gap-y-8 w-full">
+            <SkillBar
+              title="Figma"
+              :description="'業務でのフロントエンドのモック作成を率先して取り組んでいます。が、まだまだ使いこなせていないので勉強中です。'"
+              :use-period-year="2"
+            />
+          </div>
+        </div>
+        <!--  -->
       </div>
       <!-- Artifacts -->
       <div class="flex flex-col py-16 px-2 w-[960px] gap-y-4">
