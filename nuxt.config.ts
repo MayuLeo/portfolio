dfef96de4457a98a88122d27c787a65444a358ab
Leo Mayuzumi <leoleomayuzumi@gmail.com>
2023-01-18 00:23:25 +0900

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
+  ssr: false,
   app: {
-    cdnURL: "/portfolio/",
+    baseURL: "/portfolio/",
   },
   postcss: {
     plugins: {
